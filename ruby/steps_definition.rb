a7ae8e91c6c9af4904281b444552fdb50582c802
prateekbaheti <prateekb@thoughtworks.com>
2014-02-21 18:32:06 +0530

    Adding mingle project creation example scenario

--- ruby/steps_definition.rb
+++ ruby/steps_definition.rb
@@ -1,7 +1,39 @@
+require "selenium-webdriver"
+require "test/unit"
+include Test::Unit::Assertions
 
-step "test something" do
-	puts "**** executing a step"
+$browser = Selenium::WebDriver.for :firefox
+
+step "Login as {arg0} and password {arg1}" do |username, pass|
+	$browser.navigate.to("http://sabu:8080")
+	$browser.find_element(:id, "user_login").send_keys(username)
+	$browser.find_element(:id, "user_password").send_keys(pass)
+	$browser.find_element(:name, "commit").click
 end
+
+step "Create new project with name {arg0} and description {arg1}" do |project_name, description|
+	$browser.find_element(:link, "Projects").click
+	$browser.find_element(:link, "NEW PROJECT").click
+	$browser.find_element(:id, "project_name").send_keys(project_name)
+	$browser.find_element(:id, "project_description").send_keys(description)
+	$browser.find_element(:link, "CREATE PROJECT").click
+end
+
+step "Verify project {arg0} exists" do |project_name|
+	$browser.find_element(:link, "All projects").click
+	assert($browser.find_element(:link, project_name).displayed?)
+end
+
+step "Delete project" do
+	$browser.find_element(:link, "Delete this").click
+	$browser.find_element(:link, "CONTINUE TO DELETE").click
+end
+
+step "log out the user" do
+	$browser.find_element(:link, "Sign out").click
+	$browser.close
+end
+
 
 step "step that {arg0}" do |x| 
 	if (x == "fails")

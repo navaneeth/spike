b5e984718b6fba72328cac5ddfb41ea8bb885ebc
prateekbaheti <prateekb@thoughtworks.com>
2014-02-24 13:28:35 +0530

    Adding java step implementations for mingle project scenario

--- java/src/StepImplementation.java
+++ java/src/StepImplementation.java
@@ -1,10 +1,47 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import static junit.framework.Assert.assertTrue;
 
 public class StepImplementation {
 
-    @Step("test something")
-    public void testSomething() {
-        System.out.println("Inside implementation of test something step");
+    WebDriver browser = new FirefoxDriver();
+
+    @Step("Login as {arg0} and password {arg1}")
+    public void Login(String username, String password) {
+        browser.get("http://sabu:8080");
+        browser.findElement(By.id("user_login")).sendKeys(username);
+        browser.findElement(By.id("user_password")).sendKeys(password);
+        browser.findElement(By.name("commit")).click();
     }
+
+    @Step("Create new project with name {arg0} and description {arg1}")
+    public void CreateProject(String projectName, String description) {
+        browser.findElement(By.linkText("Projects")).click();
+        browser.findElement(By.linkText("NEW PROJECT")).click();
+        browser.findElement(By.id("project_name")).sendKeys(projectName);
+        browser.findElement(By.id("project_description")).sendKeys(description);
+        browser.findElement(By.linkText("CREATE PROJECT")).click();
+    }
+
+    @Step("Verify project {arg0} exists")
+    public void verifyProjectExists(String projectName) {
+        browser.findElement(By.linkText("All projects")).click();
+        assertTrue(browser.findElement(By.linkText(projectName)).isDisplayed());
+    }
+
+    @Step("Delete project")
+    public void deleteProject() {
+        browser.findElement(By.linkText("Delete this")).click();
+        browser.findElement(By.linkText("CONTINUE TO DELETE")).click();
+    }
+    @Step("log out user")
+    public void logoutUser() {
+        browser.findElement(By.linkText("Sign out")).click();
+        browser.close();
+    }
+
 
     @Step("test something with {arg0}")
     public void testSomething(String value) {
